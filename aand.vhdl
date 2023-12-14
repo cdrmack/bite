7fbe062386bc33b00a591f4be6ca423a5cf8b89b
cdrmack <git.pynvt@passmail.net>
2023-12-14 13:44:55 +0100

    minor NOT and AND refactor

--- aand.vhdl
+++ aand.vhdl
@@ -2,10 +2,16 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity aand is
-  port(i0, i1 : in std_logic; o0 : out std_logic);
+  port(
+    i0 : in std_logic;
+    i1 : in std_logic;
+    o0 : out std_logic
+    );
 end aand;
 
 architecture behav of aand is
+  signal i0_nand_i1 : std_logic;
 begin
-  o0 <= not(i0 nand i1);
+  i0_nand_i1 <= i0 nand i1;
+  o0 <= not(i0_nand_i1);
 end behav;
--- anot.vhdl
+++ anot.vhdl
@@ -2,7 +2,10 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity anot is
-  port(i0 : in std_logic; o0 : out std_logic);
+  port(
+    i0 : in std_logic;
+    o0 : out std_logic
+    );
 end anot;
 
 architecture behav of anot is
